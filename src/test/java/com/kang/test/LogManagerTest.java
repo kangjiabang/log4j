746f6fa52868c880038effb82efc987181559a82
zeqi <zeqi@wacai.com>
2018-01-08 23:07:18 +0800

    添加测试用例

---
+++ src/test/java/com/kang/test/LogManagerTest.java
@@ -0,0 +1,18 @@
+package com.kang.test;
+
+import com.kang.manager.LogManager;
+import org.junit.Test;
+
+/**
+ * @Author：zeqi
+ * @Date: Created in 21:28 8/1/18.
+ * @Description:
+ */
+public class LogManagerTest {
+
+    @Test
+    public  void testLogManagerInit() {
+
+        LogManager manager = new LogManager();
+    }
+}
