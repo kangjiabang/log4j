9a6927d357694a52e79135cfe4f43ee4d2184045
zeqi <zeqi@wacai.com>
2018-01-08 23:04:02 +0800

    log4j 增加test用例

--- src/main/java/com/kang/helpers/ConfigurationParser.java
+++ src/main/java/com/kang/helpers/ConfigurationParser.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 public class ConfigurationParser {
 
 
-    public static void parse(Properties properties) {
+    public  void parse(Properties properties) {
 
     }
 }
