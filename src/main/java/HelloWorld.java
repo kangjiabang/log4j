0b59c9141baade0003d15deb26d5f9ab64bd8a33
zeqi <zeqi@wacai.com>
2018-01-08 21:10:51 +0800

    log4j 增加test文件

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author：zeqi
+ * @Date: Created in 21:10 8/1/18.
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
